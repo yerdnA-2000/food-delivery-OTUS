317ba76d1234eb6d84243c1aee5bb5d013c3068e
yerdnA <dev.yerdna@gmail.com>
2025-03-06 21:56:48 +0700

    packages, docker, ci

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testTrueIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
